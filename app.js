a77cfc68a40e5a07f0331801a755b440b5409916
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-10-17 17:46:25 +0200

    added modules and config
    Twit and config.js added.

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
+const config = require('./config.js');
+const Twit = require('twit')
+const T = new Twit({
+  consumer_key:         config.consumer_key,
+  consumer_secret:      config.consumer_secret,
+  access_token:         config.access_token,
+  access_token_secret:  config.access_token_secret
+});
 
 app.set('view engine', 'pug');
