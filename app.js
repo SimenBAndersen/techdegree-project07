1434f7822b2dadbee865adfec4c2997257bd362a
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-10-18 12:28:52 +0200

    added files
    pug files added and minor changes to folder structure

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const config = require('./config.js');
+const bodyParser = require('body-parser');
 const Twit = require('twit')
 const T = new Twit({
   consumer_key:         config.consumer_key,
@@ -9,4 +10,14 @@ const T = new Twit({
   access_token_secret:  config.access_token_secret
 });
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+	console.log('App is running on port 3000');
+});
